c2822efc7b6438402c1e2a3537522974486b9ee6
Techuya <techu19870411@gmail.com>
2020-12-25 00:06:20 +0900

    validates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  validates :nickname, :email, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :nickname, :email, :birthday, presence: true
+  
+  validates :password, presence: true,
+            format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6}\z/ }
+  
+  validates :last_name, :first_name, presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
+  validates :last_name_kana, :first_name_kana, presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ }
 end
