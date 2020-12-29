ec08759d80edba0cbd1609009a7104ac160f4c5f
Techuya <techu19870411@gmail.com>
2020-12-29 18:05:14 +0900

    new,createアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(tweet_params)
+  end
+
+  private
+
+  def tweet_params
+    params.require(:item).permit(:name, :description, :category_id, :product_condition_id, :postage_payer_id, :prefecture_id, :arrival_day_id, :price, :user)
+  end
+
 end
