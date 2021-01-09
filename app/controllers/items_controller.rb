69cbf9504817ad770181e5edf71d1757d822486a
Techuya <techu19870411@gmail.com>
2021-01-09 21:02:24 +0900

    カテゴリーセレクトボックス実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(tweet_params)
+    Item.create(item_params)
   end
 
   private
 
-  def tweet_params
+  def item_params
     params.require(:item).permit(:name, :description, :category_id, :product_condition_id, :postage_payer_id, :prefecture_id, :arrival_day_id, :price, :user)
   end
 
