5e5cca5861d96382c72c8071d6cb73dee332ab21
Techuya <techu19870411@gmail.com>
2021-01-09 21:42:10 +0900

    配送料の負担セレクトボックス実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :product_condition
-  
+  belongs_to :postage_payer
+
   belongs_to :user
 end
