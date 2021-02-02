ec6b38a127dac27e2aa827c49e33caa60b82e6ed
Techuya <techu19870411@gmail.com>
2021-02-03 04:14:40 +0900

    コード微修正

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -11,9 +11,10 @@ class UserOrder
 
     validates :user_id
     validates :item_id
-
+  end
+  
     def save
-      order = Order.create(user_id: user_id, item: item_id)
+      order = Order.create(user_id: user_id, item_id: item_id)
       Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
     end
 end
