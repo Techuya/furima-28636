197079c7287cfa886a1d3f9048930f2405be6784
Techuya <techu19870411@gmail.com>
2021-02-03 03:00:09 +0900

    validates記述

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -3,11 +3,11 @@ class UserOrder
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :order_id
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
 
     validates :user_id
     validates :item_id
