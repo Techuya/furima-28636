57dcfa7dbf69f62098adbcd4c868e9182b18351a
Techuya <techu19870411@gmail.com>
2021-02-16 01:10:17 +0900

    rubocop実施

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -13,9 +13,9 @@ class UserOrder
     validates :item_id
     validates :token
   end
-  
-    def save
-      order = Order.create(user_id: user_id, item_id: item_id)
-      Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
-    end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
