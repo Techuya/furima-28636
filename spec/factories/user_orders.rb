2eca3347f8b76633e56f08af3f1373e87b7f40e9
Techuya <techu19870411@gmail.com>
2021-02-16 01:06:39 +0900

    user_orders / テスト実施

---
+++ spec/factories/user_orders.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user_order do
+      postal_code   { '123-4567' }
+      prefecture_id { 2 }
+      city          { '上尾市' }
+      house_number  { '本町1-1-1' }
+      building      { '本町ビル' }
+      phone_number  { '08000000000' }
+      token         { 'tok_abcdefghijk00000000000000000' }
+      user_id       { 1 }
+      item_id       { 1 }
+  end
+end
